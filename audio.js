4a267c15a4a3ca1e537ae91ca329ad85595f7f86
Joshua Cilliers <joshuac@bluebeansoftware.com>
2023-09-15 23:19:19 +0200

    deliverable 6

--- audio.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-    var audio = document.getElementById("end_credits").play();
-    audio.src = "media/closing-credits_mixdown.mp3";
-    audio.load();
-    audio.play();
-}
